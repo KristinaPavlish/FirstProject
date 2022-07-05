884a39fc5c757d0c1ab2c2a3b9a952772a723a08
KristinaPavlish <Christi0111@gmail.com>
2022-07-06 00:14:54 +0300

    Init repository

---
+++ Classes/Employee.cs
@@ -0,0 +1,66 @@
+using Microsoft.VisualBasic;
+namespace Classes;
+
+public class Employee
+{
+    private string _fullName;
+    private int _workExperience;
+    private int _numberOfHoursWorked;
+    private int _rateOfCompletedWorks;
+
+    public Employee(string fullName, int workExperience, int numberOfHoursWorked, int rateOfCompletedWorks)
+    {
+        this._fullName = fullName;
+        this._workExperience = workExperience;
+        this._numberOfHoursWorked = numberOfHoursWorked;
+        this._rateOfCompletedWorks = rateOfCompletedWorks;
+    }
+    public double CalculationOfWages(int rateOfPayPerHour)
+    {
+        double salary = _numberOfHoursWorked * rateOfPayPerHour;
+        
+        if (_numberOfHoursWorked < _rateOfCompletedWorks)
+        {
+            salary -= salary * 0.2;
+        }
+
+        if (_workExperience >= 10 && _workExperience <= 20)
+        {
+            salary += salary * 0.1;
+        }
+
+        if (_workExperience > 20)
+        {
+            salary += salary * 0.2;
+        }
+
+        return salary;
+    }
+
+    public string InitialsOfEmployee()
+    {
+        if (string.IsNullOrEmpty(_fullName))
+        {
+            return "";
+        }
+        string[] fullNameOfEmployee = _fullName.Split();
+        string firstWordOfFullName = fullNameOfEmployee.First();
+
+        string initials = "";
+        initials = firstWordOfFullName + " ";
+
+
+        string[] initialsWithoutSurname = string.Join(" ", _fullName.Split().Skip(1)).Split();
+
+        initials += string.Join("", initialsWithoutSurname.Select(x => x.Substring(0, 1).ToUpper() + "."));
+
+      /*foreach (string name in initialsWithoutSurname)
+        {
+            initials += name.Substring(0, 1).ToUpper() + ".";
+        }*/
+
+        return initials;
+    }
+
+
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,64 @@
+﻿using Classes;
+Console.WriteLine(TestTotalSalary());
+Console.WriteLine(TestNameInitials());
+Console.WriteLine(BoolTestTotalSalary()?"The salary is calculated correctly":"The salary is calculated incorrectly");
+
+ Employee FirstInitiateClass()
+{
+    // init 1 class
+    Employee employee = new Employee(null, 6, 7, 8);
+    return employee;
+}
+Employee SecondInitiateClass()
+{
+    // init 2 class
+    Employee employee = new Employee("Halushchynskyi Dmytro Ivanovych", 12, 7, 8);
+    return employee;
+}
+Employee ThirdInitiateClass()
+{
+    // init 3 class
+    Employee employee = new Employee("Ivasiuk Olena Igorovna", 23, 8, 8);
+    return employee;
+}
+
+ 
+string TestTotalSalary()
+{
+    // tests for right calculation
+    if (FirstInitiateClass().CalculationOfWages(45) == (7 * 45) - (7 * (45 * 0.2))
+        && SecondInitiateClass().CalculationOfWages(45) == 277.2 //first calculating salary ( < hours ) 7 * 45 - 7 * 45 * 0,2 = 252
+        //second calculating salary (10 > work exp < 20) 252 + 252 * 0.1 = 277.2
+        && ThirdInitiateClass().CalculationOfWages(45) == (8 * 45) + (8 * (45 * 0.2))
+       )
+        return "The salary is calculated correctly";
+    throw new Exception("The initials is calculated incorrectly");
+}
+ 
+bool BoolTestTotalSalary()
+{
+    // tests for right calculation
+    return (FirstInitiateClass().CalculationOfWages(45) == (7 * 45) - (7 * (45 * 0.2))
+            && SecondInitiateClass().CalculationOfWages(45) == 277.2
+            //first calculating salary ( < hours ) 7 * 45 - 7 * 45 * 0,2 = 252
+            //second calculating salary (10 > work exp < 20) 252 + 252 * 0.1 = 277.2
+            && ThirdInitiateClass().CalculationOfWages(45) == (8 * 45) + (8 * (45 * 0.2)));
+
+
+    // return "The salary is calculated correctly";
+    //throw new NotImplementedException();
+}
+
+string TestNameInitials ()
+ {
+     // tests for right name initials
+     if ( FirstInitiateClass().InitialsOfEmployee() == ""
+          && SecondInitiateClass().InitialsOfEmployee() == "Halushchynskyi D.I." 
+          && ThirdInitiateClass().InitialsOfEmployee() == "Ivasiuk O.I.")
+     
+         return "The initials is calculated correctly";
+
+     throw new Exception("The initials is calculated incorrectly");
+
+ }
+
